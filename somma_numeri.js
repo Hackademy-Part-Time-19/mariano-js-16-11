8e18c16bbf2baa4813272eba05c04dbbf2a26790
Gianluca Mariano <giangimar04@gmail.com>
2023-11-16 20:28:36 +0100

    Ver 1.0

--- somma_numeri.js
+++ somma_numeri.js
@@ -7,13 +7,12 @@ let b = 1;
 let somma = 0;
 
 
-for (let i = 1; i <= 200; i += 1 ){
+for (let i = 1; i <= 200; i += 1) {
 
-a = i;
-b = i;
+    a = i;
 
-somma = somma + a + b;
-
-if (i == 200) { console.log(somma)}
+    somma = somma + a;
 
 }
+
+console.log(somma)
